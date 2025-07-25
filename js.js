4697eb86f2227e924e7d6b62426d482d59c02a2d
shine <lastacc4951@gmail.com>
2025-07-25 13:23:32 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+document.getElementById('postForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    
+    const postContent = document.getElementById('postContent').value;
+    const timestamp = new Date().toLocaleString();
+
+    
+    const postElement = document.createElement('div');
+    postElement.classList.add('post');
+
+    postElement.innerHTML = `
+        <h2>You:</h2
+        <p>${postContent}</p>
+        <span class="timestamp">Posted on ${timestamp}</span>
+    `;
+    document.getElementById('postsContainer').appendChild(postElement);
+    
+    document.getElementById('postContent').value = '';
+});
